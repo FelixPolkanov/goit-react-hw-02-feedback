e2a33aed60bc30b32d0c2834eec2b661b2cc6cf8
FelixPolkanov <felixpolkanov@gmail.com>
2022-12-17 20:14:16 +0200

    add step-2

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,53 +2,91 @@ import React from 'react';
 import css from '../Feedback/feedback.module.css';
 
 class Feedback extends React.Component {
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0,
-    };
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-    onGoodBtn = () => {
-        this.setState(prevState => {
-            return {
-                good: prevState.good + 1,
-         }
-        })
-    }
+  onGoodBtn = () => {
+    this.setState(prevState => {
+      return {
+        good: prevState.good + 1,
+      };
+    });
+  };
 
-     onNeutralBtn = () => {
-        this.setState(prevState => {
-            return {
-                neutral: prevState.neutral + 1,
-         }
-        })
-    }
-     onBadBtn = () => {
-        this.setState(prevState => {
-            return {
-                bad: prevState.bad + 1,
-         }
-        })
-    }
+  onNeutralBtn = () => {
+    this.setState(prevState => {
+      return {
+        neutral: prevState.neutral + 1,
+      };
+    });
+  };
+  onBadBtn = () => {
+    this.setState(prevState => {
+      return {
+        bad: prevState.bad + 1,
+      };
+    });
+  };
 
+  countTotalFeedback = (goodFeedback, neutralFeedback, badFeedback) =>
+    goodFeedback + neutralFeedback + badFeedback;
+
+  countPositiveFeedbackPercentage = (goodFeedback, totalFeedback) => {
+    if (totalFeedback === 0 || goodFeedback === 0) {
+      return 0;
+    } else {
+      return Math.round((goodFeedback * 100) / totalFeedback);
+    }
+  };
 
   render() {
+    const total = this.countTotalFeedback(
+      this.state.good,
+      this.state.neutral,
+      this.state.bad
+    );
+    const positiveFeedback = this.countPositiveFeedbackPercentage(
+      this.state.good,
+      this.state.total
+    );
+
     return (
       <div className={css.feedback}>
         <h1 className={css.title}>Please, leave feedback</h1>
         <div className={css.controls}>
-          <button className={css.controls_btn} type="button" onClick={this.onGoodBtn}>Good</button>
-          <button className={css.controls_btn} type="button"onClick={this.onNeutralBtn}>Neutral</button>
-          <button className={css.controls_btn} type="button"onClick={this.onBadBtn}>Bad</button>
-            </div>
-            <h2 className={css.title}>Statistics:</h2>
-        <div className={css.feedback_resault}>
-                <p>Good:{this.state.good}</p>
-          <p>Neutral:{this.state.neutral}</p>
-                <p>Bad:{this.state.bad}</p>
+          <button
+            className={css.controls_btn}
+            type="button"
+            onClick={this.onGoodBtn}
+          >
+            Good
+          </button>
+          <button
+            className={css.controls_btn}
+            type="button"
+            onClick={this.onNeutralBtn}
+          >
+            Neutral
+          </button>
+          <button
+            className={css.controls_btn}
+            type="button"
+            onClick={this.onBadBtn}
+          >
+            Bad
+          </button>
         </div>
-
-        
+        <h2 className={css.statistic_title}>Statistics:</h2>
+        <div className={css.feedback_resault}>
+          <p>Good:{this.state.good}</p>
+          <p>Neutral:{this.state.neutral}</p>
+          <p>Bad:{this.state.bad}</p>
+          <p>Total:{total}</p>
+          <p>Positive feedback:{positiveFeedback}</p>
+        </div>
       </div>
     );
   }
